f0218b547a128b8773dc82af8470afc97424a363
Spring Boot User <user@example.com>
2025-12-12 14:12:25 +0800

    mod:优化

--- src/main/java/com/sunlight/invest/fund/monitor/schedule/FundMonitorScheduler.java
+++ src/main/java/com/sunlight/invest/fund/monitor/schedule/FundMonitorScheduler.java
@@ -5,7 +5,6 @@ import com.sunlight.invest.fund.monitor.service.FundMonitorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
@@ -32,7 +31,7 @@ public class FundMonitorScheduler {
      * 每天8点执行基金数据抓取和监控
      * cron表达式: 0 0 22 * * ?
      */
-    @Scheduled(cron = "0 0 8 * * ?")
+//    @Scheduled(cron = "0 0 8 * * ?")
     public void scheduledMonitorTask() {
         log.info("========== 开始执行基金监控定时任务 ==========");
         fundMonitorService.scheduledMonitorTask();
